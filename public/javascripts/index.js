97bea6d6057f5eb27407e7750432be4c6dc09ef7
Alok Khanna <akhanna02@users.noreply.github.com>
2018-04-13 16:14:46 -0500

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
     options: basicOption
   });
 
-  var ws = new WebSocket('wss://' + location.host);
+  var ws = new WebSocket('wss://' + process.env['WebSocketHost']);
   ws.onopen = function () {
     console.log('Successfully connect WebSocket');
   }
